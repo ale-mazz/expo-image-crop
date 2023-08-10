17aea6342c2c88683dc179a7bb288d5e847f0cde
Alessandro Mazzon <alessandro@forbole.com>
2023-08-10 14:06:39 +0200

    feat: fixed example app

--- source/App.tsx
+++ source/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import {
-  Dimensions, View, Image, Text, StatusBar
-} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import { Camera } from 'expo-camera';
-import ImageManipulatorView from './libraries/ImageManipulatorView';
-import { MaterialIcons as Icon } from '@expo/vector-icons';
-import HybridTouch from './HybridTouch';
-import { SaveFormat } from 'expo-image-manipulator';
+import React from "react";
+import { Dimensions, View, Image, Text, StatusBar } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { Camera } from "expo-camera";
+import ImageManipulatorView from "./libraries/ImageManipulatorView";
+import { MaterialIcons as Icon } from "@expo/vector-icons";
+import HybridTouch from "./HybridTouch";
+import { SaveFormat } from "expo-image-manipulator";
 
-const noImage = require('./assets/no_image.png');
+const noImage = require("./assets/no_image.png");
 
 export default class App extends React.Component {
   state = {
@@ -23,12 +21,15 @@ export default class App extends React.Component {
   _pickImage = async () => {
     // this.setState({ uri: null });
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status === 'granted') {
+    if (status === "granted") {
       const result = await ImagePicker.launchImageLibraryAsync();
       if (!result.canceled) {
-        this.setState({
-          uri: result.assets[0].uri,
-        }, () => this.setState({ isVisible: true }));
+        this.setState(
+          {
+            uri: result.assets[0].uri,
+          },
+          () => this.setState({ isVisible: true }),
+        );
       }
     }
   };
@@ -36,98 +37,110 @@ export default class App extends React.Component {
   _pickCameraImage = async () => {
     // this.setState({ uri: null });
     const { status } = await Camera.requestCameraPermissionsAsync();
-    if (status === 'granted') {
+    if (status === "granted") {
       const result = await ImagePicker.launchCameraAsync();
 
       if (!result.canceled) {
-        this.setState({
-          uri: result.assets[0].uri,
-        }, () => this.setState({ isVisible: true }));
+        this.setState(
+          {
+            uri: result.assets[0].uri,
+          },
+          () => this.setState({ isVisible: true }),
+        );
       }
     }
   };
 
   render() {
-    const {
-      uri, isVisible,
-    } = this.state;
-    const { width, height } = Dimensions.get('window');
+    const { uri, isVisible } = this.state;
+    const { width, height } = Dimensions.get("window");
     return (
-      <View style={{
-        backgroundColor: '#fcfcfc',
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        height,
-      }}
+      <View
+        style={{
+          backgroundColor: "#fcfcfc",
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          height,
+        }}
       >
         <StatusBar barStyle="dark-content" />
         {uri ? (
-          <Image resizeMode="contain"
+          <Image
+            resizeMode="contain"
             style={{
-              width, height, marginBottom: 40, backgroundColor: '#fcfcfc',
+              width,
+              height,
+              marginBottom: 40,
+              backgroundColor: "#fcfcfc",
             }}
             source={{ uri }}
           />
-        ) : <Image source={noImage} />}
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          backgroundColor: '#2c98fd',
-          width,
-          position: 'absolute',
-          bottom: 0,
-          padding: 20,
-        }}
+        ) : (
+          <Image source={noImage} />
+        )}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            backgroundColor: "#2c98fd",
+            width,
+            position: "absolute",
+            bottom: 0,
+            padding: 20,
+          }}
         >
-          <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickImage()}>
-            <View style={{ alignItems: 'center' }}>
+          <HybridTouch
+            style={{ flex: 1, alignItems: "center" }}
+            onPress={() => this._pickImage()}
+          >
+            <View style={{ alignItems: "center" }}>
               <Icon size={30} name="photo" color="white" />
-              <Text style={{ color: 'white', fontSize: 18 }}>Galery</Text>
+              <Text style={{ color: "white", fontSize: 18 }}>Gallery</Text>
             </View>
           </HybridTouch>
-          <HybridTouch style={{ flex: 1, alignItems: 'center' }} onPress={() => this._pickCameraImage()}>
-            <View style={{ alignItems: 'center' }}>
+          <HybridTouch
+            style={{ flex: 1, alignItems: "center" }}
+            onPress={() => this._pickCameraImage()}
+          >
+            <View style={{ alignItems: "center" }}>
               <Icon size={30} name="photo-camera" color="white" />
-              <Text style={{ color: 'white', fontSize: 18 }}>Camera</Text>
+              <Text style={{ color: "white", fontSize: 18 }}>Camera</Text>
             </View>
           </HybridTouch>
         </View>
-        {
-          uri
-          && (
-            <ImageManipulatorView
-              photo={{ uri }}
-              isVisible={isVisible}
-              onPictureChoosed={(data) => {
-                // console.log(data)
-                this.setState({ uri: data.uri });
-              }}
-              onBeforePictureChoosed={(data) => {
-                const diff = Math.abs(data.width / data.height - 16 / 9) * 1000;
-                if (data.cropped || diff < 3) {
-                  return true;
-                }
+        {uri && (
+          <ImageManipulatorView
+            photo={{ uri }}
+            isVisible={isVisible}
+            onPictureChosen={(data) => {
+              // console.log(data)
+              this.setState({ uri: data.uri });
+            }}
+            onBeforePictureChosen={(data) => {
+              const diff = Math.abs(data.width / data.height - 16 / 9) * 1000;
+              if (data.cropped || diff < 3) {
+                return true;
+              }
 
-                alert('You must crop the image.');
-                return false;
-              }}
-              // fixedMask={{ width: 200, height: 200 }}
-              onToggleModal={this.onToggleModal}
-              saveOptions={{
-                compress: 1,
-                format: SaveFormat.PNG,
-                base64: true,
-              }}
-              btnTexts={{
-                done: '決定',
-                crop: '切り抜く',
-                processing: '処理中',
-              }}
-              ratio={{ width: 16, height: 9 }}
-            />
-          )
-        }
+              alert("You must crop the image.");
+              return false;
+            }}
+            // fixedMask={{ width: 200, height: 200 }}
+            onToggleModal={this.onToggleModal}
+            saveOptions={{
+              compress: 1,
+              format: SaveFormat.PNG,
+              base64: true,
+            }}
+            btnTexts={{
+              done: "Done",
+              crop: "Crop",
+              processing: "Processing",
+            }}
+            ratio={{ width: 16, height: 9 }}
+          />
+        )}
       </View>
     );
   }
